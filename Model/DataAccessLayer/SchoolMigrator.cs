3ad1554d628650ef0b6c3638bbac97e0e0554cd5
Ali Servet Donmez <asd@pittle.org>
2016-02-29 16:44:48 +0100

    Migrate database to latest version instead of drop&create

---
+++ Model/DataAccessLayer/SchoolMigrator.cs
@@ -0,0 +1,8 @@
+﻿namespace Model.DataAccessLayer
+{
+    using Model.Migrations;
+
+    public class SchoolMigrator : System.Data.Entity.MigrateDatabaseToLatestVersion<SchoolContext, Configuration>
+    {
+    }
+}
--- Model/Migrations/Configuration.cs
+++ Model/Migrations/Configuration.cs
@@ -5,7 +5,7 @@ namespace Model.Migrations
     using System.Data.Entity.Migrations;
     using System.Linq;
 
-    internal sealed class Configuration : DbMigrationsConfiguration<Model.DataAccessLayer.SchoolContext>
+    public sealed class Configuration : DbMigrationsConfiguration<Model.DataAccessLayer.SchoolContext>
     {
         public Configuration()
         {
