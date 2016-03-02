6a78fd7e009e951721bdd69794d707073e448d10
Ali Servet Donmez <asd@pittle.org>
2016-03-02 17:56:03 +0100

    Use generator (although empty) to seed data

---
+++ Model/DataAccessLayer/SeedGenerator.cs
@@ -0,0 +1,18 @@
+﻿namespace Model.DataAccessLayer
+{
+    using System.Data.Entity;
+
+    public class SeedGenerator
+    {
+        // ReSharper disable once UnusedParameter.Local
+        public SeedGenerator(DbContext context)
+        {
+        }
+
+        public void Generate()
+        {
+            // TODO: missing implementation
+            // Ref.: https://brosteins.com/2015/08/14/generating-test-data-in-entity-framework/
+        }
+    }
+}
--- Model/Migrations/Configuration.cs
+++ Model/Migrations/Configuration.cs
@@ -4,6 +4,7 @@ namespace Model.Migrations
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
     using System.Linq;
+    using Model.DataAccessLayer;
 
     public sealed class Configuration : DbMigrationsConfiguration<Model.DataAccessLayer.SchoolContext>
     {
@@ -14,18 +15,8 @@ namespace Model.Migrations
 
         protected override void Seed(Model.DataAccessLayer.SchoolContext context)
         {
-            //  This method will be called after migrating to the latest version.
-
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
-            //  to avoid creating duplicate seed data. E.g.
-            //
-            //    context.People.AddOrUpdate(
-            //      p => p.FullName,
-            //      new Person { FullName = "Andrew Peters" },
-            //      new Person { FullName = "Brice Lambson" },
-            //      new Person { FullName = "Rowan Miller" }
-            //    );
-            //
+            var generator = new SeedGenerator(context);
+            generator.Generate();
         }
     }
 }
