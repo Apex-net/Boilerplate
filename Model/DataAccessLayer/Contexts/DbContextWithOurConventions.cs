54eecbee7ccdb04f56eb1abce460d53a4f97da6d
Ali Servet Donmez <asd@pittle.org>
2016-03-08 11:03:17 +0100

    Install ConvenientEntityFramework as a NuGet package

--- Model/DataAccessLayer/Contexts/DbContextWithOurConventions.cs
+++ Model/DataAccessLayer/Contexts/DbContextWithOurConventions.cs
@@ -2,7 +2,7 @@
 {
     using System.Data.Entity;
     using System.Data.Entity.ModelConfiguration.Conventions;
-    using Convenient.System.Data.Entity;
+    using Convenient.Data.Entity;
 
     public abstract class DbContextWithOurConventions : ConvenientDbContext
     {
